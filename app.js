5a79f0d4400a5cb10c3941ecf11ba0f97b18613f
Dangiras <dan.hausmans@gmail.com>
2022-10-17 15:25:02 +0200

    Finished Quest

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
+const mysql = require("mysql2/promise");
+const database = mysql.createPool({
+  host: process.env.DB_HOST, // address of the server
+  port: process.env.DB_PORT, // port of the DB server (mysql), not to be confused with the APP_PORT !
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -11,9 +20,12 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/user", userHandlers.getUsers);
+app.get("/api/user/:id", userHandlers.getUsersById);
 
 app.listen(port, (err) => {
   if (err) {
