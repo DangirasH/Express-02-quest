30d26447f51aefb199f67b0c7cbd88f00a5653ea
Dangiras <dan.hausmans@gmail.com>
2022-10-19 16:30:00 +0200

    filtered

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,16 +1,37 @@
 const database = require("./database");
 
 const getMovies = (req, res) => {
+  let sql = "select * from movies";
+const sqlValues = [];
+
+if (req.query.color != null) {
+  sql += " where color = ?";
+  sqlValues.push(req.query.color);
+
+  if (req.query.max_duration != null) {
+    sql += " and duration <= ?";
+    sqlValues.push(req.query.max_duration);
+  }
+} else if (req.query.max_duration != null) {
+  sql += " where duration <= ?";
+  sqlValues.push(req.query.max_duration);
+}
+
   database
-    .query("select * from movies")
+    .query(sql, sqlValues)
     .then(([movies]) => {
-      res.status(200).json(movies);
+      res.json(movies);
     })
     .catch((err) => {
       console.error(err);
       res.status(500).send("Error retrieving data from database");
     });
 };
+
+
+
+
+
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
@@ -83,6 +104,8 @@ const deleteMovie = (req, res) => {
       res.status(500).send("Error deleting the movie");
     });
 };
+
+
 
 module.exports = {
   getMovies,
